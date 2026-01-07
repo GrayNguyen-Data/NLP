f76c2e52936c5204a3b52757941bdc3ab1bdb245
GrayNguyen77 <ndtoan.work@gmail.com>
2026-01-07 10:11:40 +0700

    feat: removeStopwords

---
+++ TextPreprocessing/removeStopwords.py
@@ -0,0 +1,27 @@
+# Stopwords: Là những từ thường xuyên xuất hiện trong văn bản nhưng không mang nhiều ý nghĩa quan trọng
+
+# Đối với tiếng việt: Là các từ như "và", "là", "của", "trong", "đang", "một",...
+# Đối với tiếng anh: Là các từ như "and", "is", "the", "in", "a",...
+
+#  Sử dụng thư viện nltk = Natural Language Tookit
+# Lưu ý: Stopwords không hỗ trợ ngôn ngữ tiếng việt, thay vào đó ta có thể tạo 1 danh sách các stopwords riêng cho tiếng việt
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+STOPWORDS = set(stopwords.words("english"))
+
+def remove_stopwords_v1(text):
+    text = text.split()
+    texts = []
+    for word in text:
+        if word not in STOPWORDS:
+            texts.append(word)
+    return ' '.join(texts)
+
+# clean code 1 chút nào bro
+def remove_stopwords_v2(text):
+    return " ".join([word for word in text.split() if word not in STOPWORDS])
+if __name__ == "__main__":
+    print(remove_stopwords_v1("This is a sample sentence to demonstrate the removal of stopwords."))
+    print(remove_stopwords_v2("This is a sample sentence to demonstrate the removal of stopwords."))
+    
